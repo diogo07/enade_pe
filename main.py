1f8b8e4e8ec936776f226a7811e40d7f2d792012
diogo07 <diogosousa36@gmail.com>
2020-09-05 18:53:48 -0300

    filtrando dados por uf

--- main.py
+++ main.py
@@ -3,5 +3,8 @@ import base
 
 
 if __name__ == '__main__':
+    print('INICIANDO PROCESSAMENTO DOS DADOS ...')
+    print('ABRINDO ARQUIVO ...')
     txt = LoadTxt('{}/enade_pe/data/MICRODADOS_ENADE_2010.txt'.format(base.path))
-    txt.load()
+    enade_pernambuco_2010 = txt.load()
+
--- services/load_txt.py
+++ services/load_txt.py
@@ -1,3 +1,8 @@
+from builtins import print
+
+import pandas as pd
+import os
+from _datetime import datetime
 
 class LoadTxt:
     def __init__(self, path):
@@ -6,11 +11,20 @@ class LoadTxt:
 
 
     def load(self):
-        file = open(self.path, "r")
-        content = file.read()
-        lines = content.split('-')
-        self.header = lines[0]
-        for line in lines:
-            itens = line.split(';')
-            # if itens[8] == '26':
-            print(itens)
+        if os.path.exists(self.path):
+            tempo_inicial = datetime.now()
+            print('LENDO DADOS ...')
+            data = pd.read_csv(self.path, sep=";")
+            list = data.values.tolist()
+            dados_pernambuco = []
+            print('SEPARANDO INFORMAÇÕES IMPORTANTES ...')
+            for row in list:
+                # CÓDIGO 26 SE REFERE AO ESTADO DE PERNAMBUCO
+                if row[8] == 26:
+                    dados_pernambuco.append(row)
+            tempo_final = datetime.now()
+            tempo_gasto = tempo_final - tempo_inicial
+            print('TAREFA CONCLUÍDA COM SUCESSO EM {}'.format(tempo_gasto))
+            return dados_pernambuco
+        else:
+            print('Erro: arquivo não existe!')
